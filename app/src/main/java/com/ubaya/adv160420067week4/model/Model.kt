753712cea572550d213f96fb3c4a70b9afe0aca9
vmp <velicia.maxy.academy@gmail.com>
2023-03-21 01:23:28 +0700

    finish homework

---
+++ app/src/main/java/com/ubaya/adv160420067week4/model/Model.kt
@@ -0,0 +1,9 @@
+package com.ubaya.adv160420067week4.model
+
+data class Student(
+    val id:String?,
+    val name:String?,
+    val dob:String?,
+    val phone:String?,
+    val photoUrl:String?
+)
